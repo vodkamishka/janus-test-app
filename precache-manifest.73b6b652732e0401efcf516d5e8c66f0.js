d7fa99d0c36dba0e210e4b556cc7ea3371238feb
Vodkamishka <andrtomsk@yandex.com>
2019-07-22 17:42:34 +0700

    Updates

---
+++ precache-manifest.73b6b652732e0401efcf516d5e8c66f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102ad2cfb588675515eddbae29375527",
+    "url": "/janus-test-app/index.html"
+  },
+  {
+    "revision": "6c25d74a7312c553e6eb",
+    "url": "/janus-test-app/static/css/main.45ddb1e9.chunk.css"
+  },
+  {
+    "revision": "3264ad004c5327434d0c",
+    "url": "/janus-test-app/static/js/2.f61c64bc.chunk.js"
+  },
+  {
+    "revision": "6c25d74a7312c553e6eb",
+    "url": "/janus-test-app/static/js/main.166ac233.chunk.js"
+  },
+  {
+    "revision": "2e5d604935186398e2ea",
+    "url": "/janus-test-app/static/js/runtime~main.adb81abc.js"
+  }
+]);
